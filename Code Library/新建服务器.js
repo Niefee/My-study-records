a8119209a122b9a8e8c21c57ea3b8d5f471c2da8
沐霖 <niefee@qq.com>
2016-08-06 16:37:28 +0800

    自建服务器

---
+++ Code Library/新建服务器.js
@@ -0,0 +1,28 @@
+var http=require('http');
+var url=require('url');
+var fs=require('fs');
+http.createServer(function(req,res){
+    var urlObj=url.parse(req.url);
+    var pathname=urlObj.pathname;
+    var query=urlObj.query;
+
+    if(pathname==='/'){
+        readFileAndResponse('index.html',res);
+    }else if(pathname === '/ajax'){
+        res.end('"msg":"this is is json response"');
+    }else{
+        readFileAndResponse(pathname,res);
+    }
+}).listen(8080);
+
+function readFileAndResponse(pathname,response){
+    fs.readFile(pathname.substr(1),'utf-8',function(err,data){
+        if (err) {
+            response.writeHead(404);
+            response.end('file not found');
+        }else{
+            response.end(data);
+        }
+
+    });
+}
